b7747b560c9539d43a1d7b9f0450ce2204d05233
dugite-code <dugite-code@peekread.info>
2017-10-05 09:43:22 +0800

    Reverted inital to length back to 100

--- modoboa_imap_migration/migrations/0001_initial.py
+++ modoboa_imap_migration/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Migration',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('_password', models.CharField(max_length=255)),
+                ('_password', models.CharField(max_length=100)),
                 ('mailbox', models.ForeignKey(to='admin.Mailbox')),
             ],
             options={
